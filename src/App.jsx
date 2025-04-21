82e67496a17c8de15b927a84b9eebe8934de5604
Student <azaryanmail@gmail.com>
2025-04-21 23:02:28 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
    return (
       <>
-         <Router basename="/moviesbox/">
+         <Router basename="/moviesbox">
             <Header setCategory={setCategory} />
             <Routes>
                <Route path="/" element={<MovieList category={category} />} />
