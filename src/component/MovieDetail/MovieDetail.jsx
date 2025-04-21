d4878ded0f7fb813ab03898656118b68cca4e379
Student <azaryanmail@gmail.com>
2025-04-21 22:04:40 +0300

    add header

--- src/component/MovieDetail/MovieDetail.jsx
+++ src/component/MovieDetail/MovieDetail.jsx
@@ -6,86 +6,93 @@ import StarRating from "../StarRating/StarRating";
 import "./MovieDetail.css";
 
 const MovieDetail = () => {
-  const { id } = useParams(); // Читаем параметр id из URL
-  const navigate = useNavigate(); // Для навигации
+   const { id } = useParams(); // Читаем параметр id из URL
+   const navigate = useNavigate(); // Для навигации
 
-  const [movie, setMovie] = useState(null); //
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+   const [movie, setMovie] = useState(null); //
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchMovieDetails = async () => {
-      try {
-        const movieData = await getMovieDetails(id);
+   useEffect(() => {
+      const fetchMovieDetails = async () => {
+         try {
+            const movieData = await getMovieDetails(id);
 
-        if (movieData && movieData.title) {
-          setMovie(movieData);
-        } else {
-          setError("Фильм не найден");
-        }
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+            if (movieData && movieData.title) {
+               setMovie(movieData);
+            } else {
+               setError("Фильм не найден");
+            }
+         } catch (err) {
+            setError(err.message);
+         } finally {
+            setLoading(false);
+         }
+      };
 
-    fetchMovieDetails();
-  }, [id]);
+      fetchMovieDetails();
+   }, [id]);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+   if (loading) return <p>Loading...</p>;
+   if (error) return <p>Error: {error}</p>;
 
-  return (
-    <div className="movie-detail__wrap">
-      <div className="movie-detail">
-        {movie ? (
-          <di v className="movie-detail-content">
-            <img
-              className="movie-detail-img"
-              src={
-                movie.poster_path
-                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-                  : "https://via.placeholder.com/500"
-              }
-              alt={movie.title}
-            />
-            <div className="movie-detail-text">
-              <h1>{movie.title}</h1>
-              <p>{movie.overview}</p>
-              <p className="release-date">Release Date: {movie.release_date}</p>
+   return (
+      <div className="movie-detail__wrap">
+         <div className="movie-detail">
+            {movie ? (
+               <div className="movie-detail-content">
+                  <img
+                     className="movie-detail-img"
+                     src={
+                        movie.poster_path
+                           ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                           : "https://via.placeholder.com/500"
+                     }
+                     alt={movie.title}
+                  />
+                  <div className="movie-detail-text">
+                     <h1>{movie.title}</h1>
+                     <p>{movie.overview}</p>
+                     <p className="release-date">
+                        Release Date: {movie.release_date}
+                     </p>
 
-              {movie.homepage ? (
-                <a
-                  href={movie.homepage}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="movie-info"
-                >
-                  movie info
-                </a>
-              ) : (
-                <span className="movie-info__no">No information available</span>
-              )}
-              {/* ==================================================== */}
-              <p className="rating">
-                Rating:
-                <span>
-                  <StarRating rating={movie.vote_average} />
-                </span>
-                {movie.vote_average}
-              </p>
-              <button className="btn btn-primary" onClick={() => navigate(-1)}>
-                Back
-              </button>
-            </div>
-          </di>
-        ) : (
-          <p>Фильм не найден</p>
-        )}
+                     {movie.homepage ? (
+                        <a
+                           href={movie.homepage}
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           className="movie-info"
+                        >
+                           movie info
+                        </a>
+                     ) : (
+                        <span className="movie-info__no">
+                           No information available
+                        </span>
+                     )}
+                     {/* ==================================================== */}
+                     <p className="rating">
+                        Rating:
+                        <span>
+                           <StarRating rating={movie.vote_average} />
+                        </span>
+                        {movie.vote_average}
+                     </p>
+                     <button
+                        className="btn btn-primary"
+                        onClick={() => navigate(-1)}
+                     >
+                        Back
+                     </button>
+                  </div>
+               </div>
+            ) : (
+               <p>Фильм не найден</p>
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default MovieDetail;
